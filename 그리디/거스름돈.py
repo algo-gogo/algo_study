943446e8f9f3ce7127ff629ac53e72c37a38f1fb
yusunjo <tnswh2023@gmail.com>
2021-03-03 23:45:17 +0900

    그리디 알고리즘

---
+++ 그리디/거스름돈.py
@@ -0,0 +1,34 @@
+n = int(input())
+
+# 500 100 50 10
+
+count = 0
+
+while n > 0:
+    if n % 500 == 0:
+        count += 1
+        n -= 500
+    elif n % 100 == 0:
+        count += 1
+        n -= 100
+    elif n % 50 == 0:
+        count += 1
+        n -= 50
+    elif n % 10 == 0:
+        count += 1
+        n -= 10
+
+print(count)
+
+#######################################################################
+
+n = 1260
+count = 0
+
+coin_types = [500, 100, 50, 10]
+
+for coin in coin_types:
+    count += n // coin
+    n %= coin
+
+print(count)
---
+++ 그리디/큰수의법칙.py
@@ -0,0 +1,42 @@
+n, m, k = map(int, input().split())
+
+array = list(map(int, input().split()))
+
+array.sort()
+print(array)
+first = array[-1]
+second = array[-2]
+
+result = 0
+
+# 총 8번 최대 3번
+while m > 0:
+    for i in range(k):
+        result += first
+        m -= 1
+    result += second
+    m -= 1
+
+#####################################################################
+
+# while True:
+#     if m == 0:
+#         break
+#     for i in range(k):
+#         result += first
+#         m -= 1
+#     result += second
+#     m -=1
+
+print(result)
+
+################################################################
+
+# first 6, second 5 일 때 (6665)를 한 묶음으로 하고 4로 안나누어 떨어지면 큰수들을 더함
+
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+result += count * first
+result += (m - count) * second
+
+print(result)
