bbf7d3a21684af9c72918c48148a9adad7dc6d10
yusunjo <tnswh2023@gmail.com>
2021-03-06 18:01:45 +0900

    그리디 설탕배달(백준)

---
+++ 그리디/설탕배달.py
@@ -0,0 +1,30 @@
+# 백준 -> 알고리즘 분류 -> 그리디 -> 설탕배달
+
+n = int(input())
+
+count = 0
+
+# while n > 0:
+#     if n % 5 == 0:
+#         count += 1
+#         n -= 5
+#     elif n % 3 == 0:
+#         count += 1
+#         n -= 3
+#     else:
+#         count = -1
+#         break
+
+# while True:
+#     if n % 5 == 0:
+#         count += n // 5
+#         break
+#     else:
+#         n -= 3
+#         count += 1
+#         if n < 0:
+#             count = -1
+#             break
+#
+# print(count)
+###############################################################
