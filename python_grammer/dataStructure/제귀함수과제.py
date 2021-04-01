9f80b7379ceea6c5f378b84c637bfd115ba207a0
yusunjo <tnswh2023@gmail.com>
2021-04-01 23:19:15 +0900

    제귀함수 연습

---
+++ python_grammer/dataStructure/제귀함수과제.py
@@ -0,0 +1,30 @@
+def recursiveFunction1(n):
+    print(n)
+    if n > 0:
+        recursiveFunction1(n - 1)
+        recursiveFunction1(n - 1)
+    print('-', n)
+
+# recursiveFunction1(3)
+
+def recursiveFunction2(n):
+    print(n)
+    if n > 0:
+        recursiveFunction2(n - 1)
+        recursiveFunction2(n - 2)
+    print('-', n)
+
+# recursiveFunction2(3)
+
+def recursiveFunction3(n):
+    print(n)
+    if n > 0:
+        recursiveFunction3(n - 1)
+        recursiveFunction3(n - 2)
+        recursiveFunction3(n - 1)
+    print('-', n)
+
+recursiveFunction3(3)
+
+# 그림은 위키에
+
