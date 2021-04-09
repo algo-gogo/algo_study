ae0e3ed91de15c09669b49dc872f32b738008b16
yusunjo <tnswh2023@gmail.com>
2021-04-09 12:14:10 +0900

    다이나믹 프로그래밍 개미전사, 바닥공사

---
+++ 다이나믹프로그래밍/개미전사.py
@@ -0,0 +1,16 @@
+# 요약
+# 서로 인접한 식량창고를 건들 순 없다 -> 최소한 한칸 이상은 떨어져 있어야한다.
+# N개에 대한 정보가 주어졌을 때 얻을 수 있는 식량의 최댓값
+
+n = int(input())
+
+array = list(map(int, input().split()))
+d = [0]* 101
+
+d[0] = array[0]
+d[1] = max(array[0], array[1])
+for i in range(2, n+1):
+    d[i] = max(d[i-1], d[i-2] + array[i])
+
+print(d[n-1])
+
