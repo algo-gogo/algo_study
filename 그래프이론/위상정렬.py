0c1bc0826fe964d6a07c778844f5f9f8acb8bae8
yusunjo <tnswh2023@gmail.com>
2021-05-02 10:30:58 +0900

    그래프 이론 - 위상정렬, 크루스칼 알고리즘

---
+++ 그래프이론/위상정렬.py
@@ -0,0 +1,49 @@
+# 순서가 정해져 있는 일련의 작업을 차례대로 수행해야 할 때 사용할 수 있는 알고리즘
+# 위상정렬 - 방향 그래프의 모든 노드를 방향성에 거스르지 않도록 순서대로 나열하는 것
+
+# 1. 진입 차수가 0인 노드에 큐를 넣는다.
+# 2. 큐가 빌 때까지 과정 반복
+# - 큐에서 원소를 꺼내 해당 노드에서 출발하는 간선을 그래프에서 제거
+# - 새롭게 진입차수가 0이 된 노드를 큐에 넣음
+
+from collections import deque
+
+# 노드의 개수와 간선의 개수를 입력받기
+v, e = map(int, input().split())
+# 모든 노드에 대한 진입차수는 0으로 초기화
+indegree = [0] * (v + 1)
+graph = [[] for i in range(v+1)]
+
+# 방향 그래프의 모든 간선 정보를 입력받기
+for _ in graph(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)  # 정점 A 에서 B 로 이동가능
+    # 진입차수 1 증가
+    indegree[b] += 1
+
+# 위상 정렬 함수
+def topology_sort():
+    result = []  # 알고리즘 수행 결과를 담을 리스트
+    q = deque()  # 큐 기능을 위한 deque 라이브러리 사용
+
+    # 처음 시작할 때는 진입차수가 0인 노드를 큐에 삽입
+    for i in range(1, v+1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    # 큐가 빌 때까지 반복
+    while q:
+        # 원소꺼내기
+        now = q.popleft()
+        result.append(now)
+        # 해당 원소와 노드들의 진입차수에서 1빼기
+        for i in graph[now]:
+            indegree[i] -= 1
+            # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
+            if indegree[i] == 0:
+                q.append(i)
+
+    for i in result:
+        print(i, end=' ')
+
+topology_sort()
---
+++ 그래프이론/크루스칼.py
@@ -0,0 +1,56 @@
+# 신장 트리 - 하나의 그래프가 있을 때 모든 노드를 포함하면서 사이클이 존재하지 않는 부분 그래프
+# 최소 비용으로 만들 수 있는 신장 트리 - 크루스칼알고리즘
+
+# 1. 간선 데이터를 비용에 따라 오름차순으로 정렬
+# 2. 간선을 하나씩 확인하며 현재의 간선이 사이클을 발생시키는지 확인
+#  - 사이클이 발생하지 않는 경우 최소 신장 트리에 포함
+#  - 사이클이 발생하는 경우 최소 신장 트리에 포함시키지 않는다.
+# 3. 모든 간선에 대하여 2번의 과정을 반복
+
+# 핵심 - 가장 거리가 짧은 간선부터 차례대로 집합에 추가하면 된다.
+# 다만 같은 집합이면 안됨(사이클을 돌면 안됨)
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+# 모든 간선을 담을 리스트와 최종 비용을 담을 변수
+edges = []
+result = 0
+
+# 부모 테이블에서 , 부모를 자기 자신으로 초기화
+for i in range(1, v+1):
+    parent[i] = i
+
+# 모든 간선에 대한 정보를 입력받기
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
