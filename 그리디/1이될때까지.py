08b0c3b225f202887fe8e2b1d6fcf5bcbe243456
유순조 <tnswh2023@phpschool.com>
2021-11-02 09:43:12 +0900

    이코테 - 그리디 2

--- 그리디/1이될때까지.py
+++ 그리디/1이될때까지.py
@@ -5,17 +5,29 @@ n, k = map(int, input().split())
 # n 이 나누어지지 않으면 1을 뺀다.
 # 1을 만드는 횟수
 
+# result = 0
+#
+# while n >= k:
+#     while n % k != 0:
+#         n -= 1
+#         result += 1
+#     n = n // k
+#     result += 1
+#
+# while n > 1:
+#     n -= 1
+#     result += 1
+#
+# print(result)
+
 result = 0
-
-while n >= k:
-    while n % k != 0:
-        n -= 1
-        result += 1
-    n = n // k
-    result += 1
-
 while n > 1:
-    n -= 1
-    result += 1
+    if n % k == 0:
+        n = n // k
+        result += 1
+    else:
+        n = n - 1
+        result += 1
 
 print(result)
+
--- 그리디/숫자카드게임.py
+++ 그리디/숫자카드게임.py
@@ -1,46 +1,59 @@
+# # 행 열
+# n, m = map(int, input().split())
+#
+# array = []
+#
+# # 3 1 2
+# # 2 1 4
+# for i in range(n):
+#     array.append(list(map(int, input().split())))
+#
+# drawing = []
+#
+# for i in range(n):
+#     drawing.append(min(array[i]))
+#
+# print(max(drawing))
+#
+# # O(2n)
+#
+# #################################################################
+#
+# result = 0
+#
+# for i in range(n):
+#     data = list(map(int, input().split()))
+#     min_value = min(data)
+#     result = max(result, min_value)
+#
+# print(result)
+#
+# # O(n)
+#
+# ##########################################################
+#
+# result = 0
+#
+# for i in range(n):
+#     data = list(map(int, input().split()))
+#     min_value = 10001
+#     for j in data:
+#         min_value = min(min_value, j)
+#     result = max(result, min_value)
+#
+# print(result)
+
+###
 # 행 열
 n, m = map(int, input().split())
 
-array = []
-
-# 3 1 2
-# 2 1 4
-for i in range(n):
-    array.append(list(map(int, input().split())))
-
-drawing = []
-
-for i in range(n):
-    drawing.append(min(array[i]))
-
-print(max(drawing))
-
-# O(2n)
-
-#################################################################
 
 result = 0
-
+min_array = []
 for i in range(n):
     data = list(map(int, input().split()))
     min_value = min(data)
-    result = max(result, min_value)
+    min_array.append(min_value)
 
-print(result)
+print(max(min_array))
 
-# O(n)
-
-##########################################################
-
-result = 0
-
-for i in range(n):
-    data = list(map(int, input().split()))
-    min_value = 10001
-    for j in data:
-        min_value = min(min_value, j)
-    result = max(result, min_value)
-
-print(result)
-
-# O(
