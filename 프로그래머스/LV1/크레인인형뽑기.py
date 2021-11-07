3868aea7beb0dcc2343f1c8ee78d87a5b84dff72
YuSunjo <tnswh2023@gmail.com>
2021-11-07 16:19:27 +0900

    프로그래머스 레벨1

--- 프로그래머스/LV1/크레인인형뽑기.py
+++ 프로그래머스/LV1/크레인인형뽑기.py
@@ -1,18 +1,38 @@
+# def solution(board, moves):
+#     bucket = []
+#     answer = []
+#     for move in moves:
+#         for i in range(len(board)):
+#             if board[i][move - 1] > 0:
+#                 bucket.append(board[i][move - 1])
+#                 board[i][move - 1] = 0
+#                 if bucket[-1:] == bucket[-2:-1]:
+#                     answer += bucket[-1:]
+#                     bucket = bucket[:-2]
+#                 break
+#     return len(answer) * 2
+
 def solution(board, moves):
-    bucket = []
-    answer = []
-    for move in moves:
-        for i in range(len(board)):
-            if board[i][move - 1] > 0:
-                bucket.append(board[i][move - 1])
-                board[i][move - 1] = 0
-                if bucket[-1:] == bucket[-2:-1]:
-                    answer += bucket[-1:]
-                    bucket = bucket[:-2]
+    resultList = [0, 0]
+    result = 0
+    for i in moves:
+        for j in range(len(board)):
+            doll = board[j][i - 1]
+            if doll != 0:
+                resultList.append(doll)
+                board[j][i - 1] = 0
+                if resultList[-2] == doll:
+                    result += 2
+                    del resultList[-1]
+                    del resultList[-1]
                 break
-    return len(answer) * 2
+
+    print(resultList)
+    print(result)
+    return result
 
 
+### 4 3 1 1 3 2 4
 solution(
     [
         [0, 0, 0, 0, 0],
