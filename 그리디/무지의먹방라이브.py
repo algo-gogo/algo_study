fd06d45891b09845bd09f28c7ecbcd01d3b4d0ae
유순조 <tnswh2023@phpschool.com>
2021-11-12 18:27:28 +0900

    그리디

--- 그리디/무지의먹방라이브.py
+++ 그리디/무지의먹방라이브.py
@@ -1,34 +1,38 @@
-# def solution(food_times, k):
-#     i = 0
-#     answer = 0
-#     eatFood = 0
-#     sum(food_times)
-#     if sum(food_times) <= k:
-#         answer = -1
-#         return answer
-#     while k > 0:
-#         if i + 1 > len(food_times):
-#             i = i % 3
-#         if food_times[i] == 0:
-#             eatFood += 1
-#             if eatFood == len(food_times):
-#                 answer = -1
-#                 return answer
-#         else:
-#             food_times[i] -= 1
-#             k -= 1
-#         i += 1
-#     if food_times[(i) % 3] == 0:
-#         i += 1
-#
-#     answer = i % 3 + 1
-#     return answer
+def solution(food_times, k):
+    i = 0
+    answer = 0
+    eatFood = 0
+    sum(food_times)
+    if sum(food_times) <= k:
+        answer = -1
+        return answer
+    while k > 0:
+        if i + 1 > len(food_times):
+            i = i % 3
+        else:
+            food_times[i] -= 1
+            k -= 1
+        i += 1
+
+    loof(i, food_times)
+    # if food_times[(i) % 3] == 0:
+    #     i += 1
+
+    answer = i % 3 + 1
+    return answer
+
+def loof(i, food_times):
+    if food_times[i % 3] == 0:
+        return loof(i + 1, food_times)
+    return i
+
+
 
 
 # solution([3,1,2], 5)
 # solution([5, 3, 5], 10)
-# print(solution([3, 1, 2], 5))
-# print(solution([5, 3, 5], 10))
+print(solution([3, 1, 2], 5))
+print(solution([5, 3, 5], 10))
 
 
 ## 인터넷
@@ -62,33 +66,33 @@
 # print(solution([3, 1, 2], 5))
 # solution([3, 1, 2], 5)
 
-import heapq
-
-
-def solution(food_times, k):
-    if sum(food_times) <= k:
-        return -1
-
-    q = []
-    for i in range(len(food_times)):
-        heapq.heappush(q, (food_times[i], i + 1))  # (3, 1) (1, 2) (2, 3)
-
-    sum_value = 0
-    previous = 0
-    length = len(food_times)
-    while sum_value + ((q[0][0] - previous) * length) <= k:
-        now = heapq.heappop(q)[0]
-        sum_value += (now - previous) * length
-        length -= 1
-        previous = now
-
-    ## tuple sort
-    result = sorted(q, key=lambda x: x[1])
-    print(result)
-    return result[(k - sum_value) % length][1]
-
-
-# def h(x):
-#     return x[1]
-print(solution([3, 1, 2], 5))
-print(solution([20, 5, 5], 10))
+# import heapq
+#
+#
+# def solution(food_times, k):
+#     if sum(food_times) <= k:
+#         return -1
+#
+#     q = []
+#     for i in range(len(food_times)):
+#         heapq.heappush(q, (food_times[i], i + 1))  # (3, 1) (1, 2) (2, 3)
+#
+#     sum_value = 0
+#     previous = 0
+#     length = len(food_times)
+#     while sum_value + ((q[0][0] - previous) * length) <= k:
+#         now = heapq.heappop(q)[0]
+#         sum_value += (now - previous) * length
+#         length -= 1
+#         previous = now
+#
+#     ## tuple sort
+#     result = sorted(q, key=lambda x: x[1])
+#     print(result)
+#     return result[(k - sum_value) % length][1]
+#
+#
+# # def h(x):
+# #     return x[1]
+# print(solution([3, 1, 2], 5))
+# print(solution([20, 5, 5], 10))
