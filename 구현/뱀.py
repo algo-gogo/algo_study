7003f24daebfc83eb9316bbbcb6e0efb5aeca42a
유순조 <tnswh2023@phpschool.com>
2021-11-19 11:23:32 +0900

    이코테 - chapter12 - 구현

--- 구현/뱀.py
+++ 구현/뱀.py
@@ -8,13 +8,27 @@
 #                       15 L
 #                       17 D
 
+### 전체적인 맵을 그림
+### 방향으로 한칸 움직임
+### snake를 큐로 만듬
+### 만약 사과가 있다면 snake 를 추가
+### 만약 사과가 없다면 snake 현재 좌표를 추가하고 popleft()를 통해 처음에 들어온 좌표 없애줌
+
+from collections import deque
+
 n = int(input())
 k = int(input())
+
+# 전체적인 맵
+array = [[0 for i in range(n)] for j in range(n)]
+
 appleList = []
 for i in range(k):
     a = list(map(int, input().split()))
     a[0] = a[0] - 1
     a[1] = a[1] - 1
+    # 맵에 사과좌표를 넣음
+    array[a[0]][a[1]] = 1
     appleList.append(a)
 
 directList = []
@@ -26,9 +40,9 @@ for i in range(directCount):
 # 이거 왜 정렬이 안되는거야 **
 directList.sort(key=lambda x: x[0], reverse=True)
 
-array = [[0 for i in range(n)] for j in range(n)]
 # print(array)
 # 우 상 좌 하
+snake = deque([[0, 0]])
 dx = [0, -1, 0, 1]
 dy = [1, 0, -1, 0]
 startX = 0
@@ -39,20 +53,49 @@ result = 0
 breakPoint = 0
 direct = 0
 print(directList)
+
+
+def directionTurn(direction, rl):
+    if direction == 0:
+        direction = 4
+    if rl == 'D':
+        direction = (direction - 1) % 4
+    else:
+        direction = (direction + 1) % 4
+    return direction
+
+
 for i in range(directCount):
     for j in range(int(directList[i][0])):
         nx = startX + dx[direct]
         ny = startY + dy[direct]
         result += 1
-        # 벽에 부딫힐 경우
+        # 벽에 부딫히지 않을 경우
         if 0 <= nx < n and 0 <= ny < n:
+            # for appleX, appleY in appleList:
+            #     if appleX == nx and appleY == ny:
+            #         body = 1
+            if [nx, ny] in snake:
+                breakPoint = -1
+                break
+            ### 사과가 있을 경우
+            if array[nx][ny] == 1:
+                array[nx][ny] = 0
+                snake.append([nx, ny])
+            ### 사과가 없을 경우
+            elif array[nx][ny] == 0:
+                snake.append([nx, ny])
+                snake.popleft()
             startX = nx
             startY = ny
+
             continue
+        # 벽에 부딫힐 경우
         else:
             breakPoint = -1
             break
     # 우 상 좌 하
+    # directionTurn(direct, directList[i][1])
     if directList[i][1] == 'D' and direct == 0:
         direct = 3
     elif directList[i][1] == 'L' and direct == 0:
@@ -71,6 +114,9 @@ for i in range(directCount):
         direct = 0
     if breakPoint == -1:
         break
+
+# ----------------------------------------------------
+
 
 
 print(result)
--- 구현/치킨배달.py
+++ 구현/치킨배달.py
@@ -2,11 +2,7 @@
 # 1 - 집
 # 2 - 치킨집
 
-
-def twoListSelect(twoList, b):
-    if len(twoList) == b:
-        return twoList
-    # 폐업해야하는 가게 찾기...
+from itertools import combinations
 
 a, b = map(int, input().split())
 
@@ -15,30 +11,38 @@ for i in range(a):
     aa = list(map(int, input().split()))
     array.append(aa)
 
-oneList = []
-twoList = []
+# 1 - 집
+houseList = []
+# 2 - 치킨
+chickenList = []
 print(array)
 for i in range(a):
     for j in range(a):
         if array[i][j] == 1:
-            oneList.append((i, j))
+            houseList.append((i, j))
         if array[i][j] == 2:
-            twoList.append((i, j))
+            chickenList.append((i, j))
 
-print(oneList)
-print(twoList)
-twoListSelect(twoList, b)
-distanceList = []
-resultList = []
-for a, b in twoList:
-    for x, y in oneList:
-        distance = abs(a - x) + abs(b - y)
-        distanceList.append(distance)
-    resultList.append(min(distanceList))
-    distanceList = []
-print(resultList)
-resultList.sort()
-print(resultList)
-result = sum(resultList[:b])
-print(result)
+print("house", houseList)
+print("chicken", chickenList)
+chickenListSelect = list(combinations(chickenList, b))
 
+print("chickenListSelect", chickenListSelect)
+
+resultList = [[0] for i in range(len(chickenListSelect))]
+print("resultList", resultList)
+for house in houseList:
+    for chickenSelect in range(len(chickenListSelect)):
+        distanceList = []
+        for i in range(b):
+            distance = abs(house[0] - chickenListSelect[chickenSelect][i][0]) + abs(house[1] - chickenListSelect[chickenSelect][i][1])
+            distanceList.append(distance)
+        chickenDistance = min(distanceList)
+        resultList[chickenSelect].append(chickenDistance)
+
+print(resultList)
+minList = []
+for i in resultList:
+    minList.append(sum(i))
+
+print(min(minList))
