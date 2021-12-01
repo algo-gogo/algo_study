bf0cab409538e2461dcaeaf7dc0f67efa708d77a
유순조 <tnswh2023@phpschool.com>
2021-12-01 14:22:27 +0900

    이코테 - chapter11 - 구현

--- 구현/뱀.py
+++ 구현/뱀.py
@@ -14,110 +14,6 @@
 ### 만약 사과가 있다면 snake 를 추가
 ### 만약 사과가 없다면 snake 현재 좌표를 추가하고 popleft()를 통해 처음에 들어온 좌표 없애줌
 
-# from collections import deque
-#
-# n = int(input())
-# k = int(input())
-#
-# # 전체적인 맵
-# array = [[0 for i in range(n)] for j in range(n)]
-#
-# appleList = []
-# for i in range(k):
-#     a = list(map(int, input().split()))
-#     a[0] = a[0] - 1
-#     a[1] = a[1] - 1
-#     # 맵에 사과좌표를 넣음
-#     array[a[0]][a[1]] = 1
-#     appleList.append(a)
-#
-# directList = []
-# directCount = int(input())
-# for i in range(directCount):
-#     a = list(map(str, input().split()))
-#     directList.append(a)
-#
-# directList.sort(key=lambda x: int(x[0]))
-# print(directList)
-# print(array)
-# # 우 상 좌 하
-# snake = deque([[0, 0]])
-# dx = [0, -1, 0, 1]
-# dy = [1, 0, -1, 0]
-# startX = 0
-# startY = 0
-#
-# result = 0
-#
-# breakPoint = 0
-# direct = 0
-# print(directList)
-#
-#
-# def directionTurn(direction, rl):
-#     if direction == 0:
-#         direction = 4
-#     if rl == 'D':
-#         direction = (direction - 1) % 4
-#     else:
-#         direction = (direction + 1) % 4
-#     return direction
-#
-#
-# for i in range(directCount):
-#     for j in range(int(directList[i][0])):
-#         nx = startX + dx[direct]
-#         ny = startY + dy[direct]
-#         result += 1
-#         # 벽에 부딫히지 않을 경우
-#         if 0 <= nx < n and 0 <= ny < n:
-#             # for appleX, appleY in appleList:
-#             #     if appleX == nx and appleY == ny:
-#             #         body = 1
-#             if [nx, ny] in snake:
-#                 breakPoint = -1
-#                 break
-#             ### 사과가 있을 경우
-#             if array[nx][ny] == 1:
-#                 array[nx][ny] = 0
-#                 snake.append([nx, ny])
-#             ### 사과가 없을 경우
-#             elif array[nx][ny] == 0:
-#                 snake.append([nx, ny])
-#                 snake.popleft()
-#             startX = nx
-#             startY = ny
-#
-#             continue
-#         # 벽에 부딫힐 경우
-#         else:
-#             breakPoint = -1
-#             break
-#     # 우 상 좌 하
-#     # directionTurn(direct, directList[i][1])
-#     if directList[i][1] == 'D' and direct == 0:
-#         direct = 3
-#     elif directList[i][1] == 'L' and direct == 0:
-#         direct = 1
-#     elif directList[i][1] == 'D' and direct == 1:
-#         direct = 0
-#     elif directList[i][1] == 'L' and direct == 1:
-#         direct = 2
-#     elif directList[i][1] == 'D' and direct == 2:
-#         direct = 1
-#     elif directList[i][1] == 'L' and direct == 2:
-#         direct = 3
-#     elif directList[i][1] == 'D' and direct == 3:
-#         direct = 2
-#     elif directList[i][1] == 'L' and direct == 3:
-#         direct = 0
-#     if breakPoint == -1:
-#         break
-#
-# print(result)
-
-# ----------------------------------------------------
-
 from collections import deque
 
 n = int(input())
@@ -135,55 +31,139 @@ for i in range(k):
     array[a[0]][a[1]] = 1
     appleList.append(a)
 
-change = {}
-l = int(input())
-for _ in range(l):
-    x, c = input().split()
-    change[int(x)] = c
+directList = []
+directCount = int(input())
+for i in range(directCount):
+    a = list(map(str, input().split()))
+    directList.append(a)
 
+directList.sort(key=lambda x: int(x[0]))
+print("directList", directList)
+print("array", array)
+# 우 상 좌 하
 snake = deque([[0, 0]])
+dx = [0, -1, 0, 1]
+dy = [1, 0, -1, 0]
+startX = 0
+startY = 0
 
-# 북 동 남 서
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+result = 0
 
+breakPoint = 0
+direct = 0
 
-d = 1
-times = 0
-nx, ny = 0, 0
-
-def boundary_check(x, y):
-    if 0 <= x < n and 0 <= y < n:
-        return True
+def directionTurn(direction, rl):
+    print("direction", direction)
+    print("rl", rl)
+    if direction == 0:
+        direction = 4
+    if rl == 'D':
+        direction = (direction - 1) % 4
     else:
-        return False
+        direction = (direction + 1) % 4
+    return direction
 
-def change_direction(direction, d):
-    if direction == 'D':
-        d = (d + 1) % 4
-    else:
-        d = (d - 1) % 4
-    return d
-
+### 0, 1, 2
+directCountCount = 0
 while True:
-    times += 1
-    nx += dx[d]
-    ny += dy[d]
-    if times in change.keys():
-        d = change_direction(change[times], d)
+    print("directCountCount", directCountCount)
+    result += 1
+    nx = startX + dx[direct]
+    ny = startY + dy[direct]
 
-    if boundary_check(nx, ny):
+    if directCountCount < directCount:
+        if result == int(directList[directCountCount][0]):
+            direct = directionTurn(direct, directList[directCountCount][1])
+            directCountCount += 1
+    if 0 <= nx < n and 0 <= ny < n:
         if [nx, ny] in snake:
+            breakPoint = -1
             break
+        ### 사과가 있을 경우
         if array[nx][ny] == 1:
             array[nx][ny] = 0
             snake.append([nx, ny])
-
+        ### 사과가 없을 경우
         elif array[nx][ny] == 0:
             snake.append([nx, ny])
-            x, y = snake.popleft()
+            snake.popleft()
+            print("snake", snake)
+        startX = nx
+        startY = ny
+
+        continue
+    # 벽에 부딫힐 경우
     else:
+        breakPoint = -1
         break
-print(times)
+print(result)
 
+# ----------------------------------------------------
 
+# from collections import deque
+#
+# n = int(input())
+# k = int(input())
+#
+# # 전체적인 맵
+# array = [[0 for i in range(n)] for j in range(n)]
+#
+# appleList = []
+# for i in range(k):
+#     a = list(map(int, input().split()))
+#     a[0] = a[0] - 1
+#     a[1] = a[1] - 1
+#     # 맵에 사과좌표를 넣음
+#     array[a[0]][a[1]] = 1
+#     appleList.append(a)
+#
+# change = {}
+# l = int(input())
+# for _ in range(l):
+#     x, c = input().split()
+#     change[int(x)] = c
+#
+# snake = deque([[0, 0]])
+#
+# # 북 동 남 서
+# dx = [-1, 0, 1, 0]
+# dy = [0, 1, 0, -1]
+#
+#
+# d = 1
+# times = 0
+# nx, ny = 0, 0
+#
+# def boundary_check(x, y):
+#     if 0 <= x < n and 0 <= y < n:
+#         return True
+#     else:
+#         return False
+#
+# def change_direction(direction, d):
+#     if direction == 'D':
+#         d = (d + 1) % 4
+#     else:
+#         d = (d - 1) % 4
+#     return d
+#
+# while True:
+#     times += 1
+#     nx += dx[d]
+#     ny += dy[d]
+#     if times in change.keys():
+#         d = change_direction(change[times], d)
+#
+#     if boundary_check(nx, ny):
+#         if [nx, ny] in snake:
+#             break
+#         if array[nx][ny] == 1:
+#             array[nx][ny] = 0
+#             snake.append([nx, ny])
+#
+#         elif array[nx][ny] == 0:
+#             snake.append([nx, ny])
+#             x, y = snake.popleft()
+#     else:
+#         break
+# print(times)
--- 구현/자물쇠와열쇠.py
+++ 구현/자물쇠와열쇠.py
@@ -1,85 +1,50 @@
-def isUnLock(keyMoveList, lockList):
-    removeList = []
-
-    for i in lockList:
-        for j in keyMoveList:
-            if i == j:
-                removeList.append(i)
-                if len(removeList) == len(lockList):
-                    return True
+def turn(arr):
+    n = len(arr)
+    keyList = []
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            keyList.append([-j, i])
+    return keyList
 
 
-def findKey(keyList, lockList):
-    length = len(lockList)
+def check(startX, startY, key, lock, expendSize, start, end):
+    expendList = [[0] * expendSize for _ in range(expendSize)]
 
-    # 큰 사각형으로 봤을 경우 맨 왼쪽 위로 이동
-    for i in range(len(keyList)):
-        keyList[i][0] = keyList[i][0] - length * 2
-        keyList[i][1] = keyList[i][1] - length * 2
-
-    for i in range(length * 2):
-        # print( "keyList", keyList)
-        for j in range(length * 2):
-            keyMoveList = []
-            newKeyList = [[0 for _ in range(len(keyList))] for _ in range(len(keyList))]
-            for s in range(len(keyList)):
-                newKeyList[s][0] = keyList[s][0] + 1
-                newKeyList[s][1] = keyList[s][1] + 1
-            keyMoveList.append(newKeyList)
-            if isUnLock(keyMoveList, lockList):
-                return True
-        for k in range(len(keyList)):
-            keyList[k][0] = keyList[k][0] + 1
-
-    # keyMoveList = []
-    # for j in range(length * 2):
-    #     for i in range(len(keyList)):
-    #         keyList[i][0] = keyList[i][0] + 1
-    #         keyList[i][1] = keyList[i][1] + 1
-    #     keyMoveList.append(keyList)
-    #     if isUnLock(keyMoveList, lockList):
-    #         return True
-    #     for k in range(length):
-    #         keyList[k][0] = keyList[k][0] + 1
-
-
-def solution(key, lock):
-    keyList1 = []
-    keyList2 = []
-    keyList3 = []
-    keyList4 = []
     for i in range(len(key)):
         for j in range(len(key)):
-            if key[i][j] == 1:
-                keyList1.append([i, j])
-                # 90도 회전
-                keyList2.append([-j, i])
-                # 180도 회전
-                keyList3.append([-i, -j])
-                # 270도 회전
-                keyList4.append([j, -i])
-    lockList = []
-    for i in range(len(lock)):
-        for j in range(len(lock)):
-            if lock[i][j] == 0:
-                lockList.append([i, j])
+            expendList[startX + i][startY + j] += key[i][j]
 
-    if findKey(keyList1, lockList):
-        return True
-    if findKey(keyList2, lockList):
-        return True
-    if findKey(keyList3, lockList):
-        return True
-    if findKey(keyList4, lockList):
-        return True
+    for i in range(start, end):
+        for j in range(start, end):현
+            expendList[i][j] += lock[i - start][j - start]
+            if expendList[i][j] != 1:
+                return False
 
-    print(keyList1)
-    print(keyList2)
-    print(keyList3)
-    print(keyList4)
+    return True
 
-    answer = False
-    return answer
+def rotation(arr):
+    n = len(arr)
+    ret = [[0] * n for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            ret[j][n-1-i] = arr[i][j]
+    return ret
+
+def solution(key, lock):
+    # 가로
+    start = len(key[0]) - 1
+    end = start + len(lock)
+    expendSize = len(lock) + start * 2
+
+    for a in range(4):
+        for i in range(end):
+            for j in range(end):
+                if check(i, j, key, lock, expendSize, start, end):
+                    return True
+        key = rotation(key)
+
+    return False
 
 
 print(solution(
