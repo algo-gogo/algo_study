8e47c2ba65aee255fa9dd70b724013a661510ce4
YuSunjo <tnswh2023@gmail.com>
2021-12-11 17:31:48 +0900

    이코테 chapter13 - 정렬

---
+++ 정렬/안테나.py
@@ -0,0 +1,23 @@
+n = int(input())
+numList = list(map(int, input().split()))
+
+numList.sort()
+
+print(numList)
+#
+# resultList = []
+# for i in range(len(numList)):
+#     result = 0
+#     for j in range(len(numList)):
+#         result += abs(numList[i] - numList[j])
+#     resultList.append(result)
+#
+# print(resultList)
+# print(min(resultList))
+
+resultList = [0] * n
+for i in range(len(numList)):
+    resultList[i] = sum(numList) - n * numList[i]
+
+print(resultList)
+print(min(resultList))
