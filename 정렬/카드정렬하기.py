7f6014695fdf3682c839ec0dee2111ab153daeee
ballde <67853463+YuSunjo@users.noreply.github.com>
2021-12-14 11:17:26 +0900

    이코테 - chapter13 - 정렬 (#8)
    Co-authored-by: 유순조 <tnswh2023@phpschool.com>

--- 정렬/카드정렬하기.py
+++ 정렬/카드정렬하기.py
@@ -1,31 +1,51 @@
 # https://www.acmicpc.net/problem/1715
 
+# n = int(input())
+# nList = []
+# for i in range(n):
+#     a = int(input())
+#     nList.append(a)
+#
+# nList.sort()
+#
+# print(nList)
+# # result = 0
+# # length = len(nList)
+# # for i in range(length - 1):
+# #     nList[i + 1] = nList[i] + nList[i + 1]
+# #     result += nList[i + 1]
+# #
+# # print(result)
+#
+# result = 0
+# length = len(nList)
+# sumN = 0
+# for i in range(length - 1):
+#     sumN = nList[0] + nList[1]
+#     del nList[0]
+#     del nList[0]
+#     nList.append(sumN)
+#     result += sumN
+#     nList.sort()
+#
+# print(result)
+
+import heapq
+
 n = int(input())
 nList = []
 for i in range(n):
     a = int(input())
-    nList.append(a)
-
-nList.sort()
+    heapq.heappush(nList, a)
 
 print(nList)
-# result = 0
-# length = len(nList)
-# for i in range(length - 1):
-#     nList[i + 1] = nList[i] + nList[i + 1]
-#     result += nList[i + 1]
-#
-# print(result)
 
 result = 0
 length = len(nList)
 sumN = 0
 for i in range(length - 1):
-    sumN = nList[0] + nList[1]
-    del nList[0]
-    del nList[0]
-    nList.append(sumN)
+    sumN = heapq.heappop(nList) + heapq.heappop(nList)
+    heapq.heappush(nList, sumN)
     result += sumN
-    nList.sort()
 
 print(result)
