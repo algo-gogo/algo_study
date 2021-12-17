a8f4ab2703dd417d5f0a9819584d4e06e476b809
ballde <67853463+YuSunjo@users.noreply.github.com>
2021-12-17 14:28:48 +0900

    이코테 - chapter12 - bfs, dfs (#9)
    Co-authored-by: 유순조 <tnswh2023@phpschool.com>

--- BFS_DFS/특정거리의도시찾기.py
+++ BFS_DFS/특정거리의도시찾기.py
@@ -7,53 +7,51 @@
 # 1 3
 # 2 3
 # 2 4
-
-from collections import deque
-
-n, m, k, x = map(int, input().split())
-
-graph = [[] for _ in range(n+1)]
-distance = [0] * (n + 1)
-visited = [False] * (n + 1)
-
-# 1, 2, 3, 4 도시에서 뻗어가는 경로
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-
-print(graph)
-print(distance)
-print(visited)
-
-
-# 최단거리, 출발도시
-def bfs(dist, start):
-    q = deque()
-    q.append(start)
-    answer = []
-    while q:
-        now = q.popleft()
-        for i in graph[now]:
-            if not visited[i]:
-                visited[i] = True
-                q.append(i)
-                distance[i] = distance[i] + 1
-                if distance[i] == dist:
-                    answer.append(i)
-
-    if len(answer) == 0:
-        print(-1)
-    else:
-        for i in answer:
-            answer.sort()
-            print(answer)
-
-    return
-
-
-bfs(k, x)
-
-
+#
+# from collections import deque
+#
+# n, m, k, x = map(int, input().split())
+#
+# graph = [[] for _ in range(n+1)]
+# distance = [0] * (n + 1)
+# visited = [False] * (n + 1)
+#
+# # 1, 2, 3, 4 도시에서 뻗어가는 경로
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     graph[a].append(b)
+#
+# print(graph)
+# print(distance)
+# print(visited)
+#
+#
+# # 최단거리, 출발도시
+# def bfs(dist, start):
+#     q = deque()
+#     q.append(start)
+#     answer = []
+#     while q:
+#         now = q.popleft()
+#         for i in graph[now]:
+#             if not visited[i]:
+#                 visited[i] = True
+#                 q.append(i)
+#                 distance[i] = distance[i] + 1
+#                 if distance[i] == dist:
+#                     answer.append(i)
+#
+#     if len(answer) == 0:
+#         print(-1)
+#     else:
+#         for i in answer:
+#             answer.sort()
+#             print(answer)
+#
+#     return
+#
+#
+# bfs(k, x)
 
 
 # def bfs(start):
@@ -78,3 +76,48 @@ bfs(k, x)
 #         for i in answer:
 #             print(i)
 
+
+###############################
+
+from collections import deque
+
+n, m, k, x = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+distance = [0] * (n + 1)
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+print(graph)
+print(visited)
+print(distance)
+
+
+def bfs(start):
+    resultList = []
+    queue = deque()
+    queue.append(start)
+    visited[start] = True
+    distance[start] = 0
+    while queue:
+        now = queue.popleft()
+        for i in graph[now]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+                distance[i] = distance[now] + 1
+                if distance[i] == k:
+                    resultList.append(i)
+
+    if len(resultList) == 0:
+        print(-1)
+    else:
+        resultList.sort()
+        for result in resultList:
+            print(result)
+
+
+bfs(x)
