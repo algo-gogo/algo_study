42eaeed23c925dc3ee15f9477039422369946aa0
ballde <67853463+YuSunjo@users.noreply.github.com>
2021-12-28 11:43:54 +0900

    이코테 - chapter15 - 이진탐색 (#14)
    Co-authored-by: 유순조 <tnswh2023@phpschool.com>

--- 이진탐색/공유기설치.py
+++ 이진탐색/공유기설치.py
@@ -1,4 +1,26 @@
+# from itertools import combinations
+#
+# n, c = map(int, input().split())
+#
+# nList = []
+# for i in range(n):
+#     nList.append(int(input()))
+#
+# nList.sort()
+#
+# print(nList)
+#
+# selectN = list(combinations(nList, 3))
+# print(selectN)
+#
+# resultList = []
+# for n1, n2, n3 in selectN:
+#     result = 0
+#     resultList.append(min(abs(n1 - n2), abs(n2 - n3)))
+#
+# print(max(resultList))
 
+# ---------------------------------------------------------
 n, c = map(int, input().split())
 
 nList = []
@@ -8,3 +30,9 @@ for i in range(n):
 nList.sort()
 
 print(nList)
+
+def binarySearch(array, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+    return False
