571f357ed99ad219ad20ede8f567acd08bffe720
유순조 <tnswh2023@phpschool.com>
2022-01-14 09:28:43 +0900

    이코테 - chapter16 - 다이나믹 프로그래밍 정리

--- 다이나믹프로그래밍/병사배치하기.py
+++ 다이나믹프로그래밍/병사배치하기.py
@@ -11,10 +11,9 @@ print(d)
 for i in range(n):
     for j in range(i):
         if nList[j] > nList[i]:
-            d[i] = max(d[j], d[j] + 1)
+            d[i] = max(d[i], d[j] + 1)
 
 print(n - max(d))
-
 
 # 7
 # 15 11 4 8 5 2 4
