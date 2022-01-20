09e7e0625f0b679dddc13808d652816eee25ffd7
ballde <67853463+YuSunjo@users.noreply.github.com>
2022-01-20 12:06:15 +0900

    이코테 - chapter17 - 최단거리 (#23)
    Co-authored-by: 유순조 <tnswh2023@phpschool.com>

--- 최단경로/숨바꼭질.py
+++ 최단경로/숨바꼭질.py
@@ -1,10 +1,31 @@
 # https://www.acmicpc.net/problem/1697
+# 메모리 초과
+
+from collections import deque
+
+d = [-1, 1, 2]
+
+def bfs(n, k):
+    queue = deque()
+    queue.append((n, 0))
+    while queue:
+        n, count = queue.popleft()
+        if n == k:
+            return count
+        for i in range(3):
+            if i == 2:
+                nx = n * 2
+            else:
+                nx = n + d[i]
+
+            if 0 <= nx:
+                queue.append((nx, count + 1))
+
 
 INF = int(1e9)
 n, k = map(int, input().split())
 
-maxNK = max(n, k)
+print(bfs(n, k))
 
-graph = [[INF] * (maxNK + 1) for _ in range(maxNK + 1)]
 
-print(graph)
+
