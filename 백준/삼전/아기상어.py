8e41d97d0687196ddc9f9d32d2bfcbcbd61dbb26
ballde <67853463+YuSunjo@users.noreply.github.com>
2022-02-22 22:17:08 +0900

    이코테 - 아기상어 (#32)
    Co-authored-by: 유순조 <tnswh2023@phpschool.com>

--- 백준/삼전/아기상어.py
+++ 백준/삼전/아기상어.py
@@ -1,5 +1,104 @@
 # https://www.acmicpc.net/problem/16236
 
+# from collections import deque
+#
+# # 3
+# # 0 0 0
+# # 0 0 0
+# # 0 9 0
+#
+# n = int(input())
+# graph = []
+# fishList = []
+#
+# # 상 좌 하 우
+# dx = [-1, 0, 1, 0]
+# dy = [0, -1, 0, 1]
+#
+# for i in range(n):
+#     l = list(map(int, input().split()))
+#     graph.append(l)
+#
+# sharkX, sharkY = 0, 0
+# for i in range(n):
+#     for j in range(n):
+#         if graph[i][j] == 9:
+#             sharkX = i
+#             sharkY = j
+#         if 1 <= graph[i][j] <= 6:
+#             fishList.append((graph[i][j], i, j))
+#
+# print(sharkX, sharkY)
+# print(graph)
+# fishList.sort()
+# print(fishList)
+#
+# # 상어의 크기
+# babyShark = 2
+# # 물고기 먹은 개수
+# fish = 0
+# timeList = []
+#
+#
+# # def bfs(x, y):
+# #     global babyShark
+# #     global fish
+# #     global timeList
+# #     queue = deque()
+# #     queue.append((x, y, 0))
+# #     visited = [[False] * n for _ in range(n)]
+# #     while queue:
+# #         x, y, time = queue.popleft()
+# #         timeList.append(time)
+# #         for direction in range(4):
+# #             nx = x + dx[direction]
+# #             ny = y + dy[direction]
+# #             if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+# #                 visited[nx][ny] = True
+# #                 if 0 < graph[nx][ny] < babyShark:
+# #                     fish += 1
+# #                     queue.append((nx, ny, time + 1))
+# #                     if fish == babyShark:
+# #                         fish = 0
+# #                         babyShark += 1
+# #                 elif 0 < graph[nx][ny] == babyShark:
+# #                     queue.append((nx, ny, time))
+# #                 else:
+# #                     queue.append((nx, ny, time))
+#
+#
+# def move(x, y, destinationX, destinationY):
+#     global babyShark
+#     queue = deque()
+#     queue.append((x, y, 0))
+#     visited = [[False] * n for _ in range(n)]
+#     while queue:
+#         x, y, time = queue.popleft()
+#         if x == destinationX and y == destinationY:
+#             return time
+#         for direction in range(4):
+#             nx = x + dx[direction]
+#             ny = y + dy[direction]
+#             if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+#                 visited[nx][ny] = True
+#                 if graph[nx][ny] <= babyShark:
+#                     queue.append((nx, ny, time + 1))
+#
+#
+# for fishSize, fx, fy in fishList:
+#     if fishSize < babyShark:
+#         result = move(sharkX, sharkY, fx, fy)
+#         print(result)
+#         timeList.append(result)
+#         sharkX = fx
+#         sharkY = fy
+#         fish += 1
+#         if fish == babyShark:
+#             fish = 0
+#             babyShark += 1
+#
+# print(timeList)
+
 from collections import deque
 
 # 3
@@ -9,6 +108,7 @@ from collections import deque
 
 n = int(input())
 graph = []
+fishList = []
 
 # 상 좌 하 우
 dx = [-1, 0, 1, 0]
@@ -24,38 +124,66 @@ for i in range(n):
         if graph[i][j] == 9:
             sharkX = i
             sharkY = j
+        if 1 <= graph[i][j] <= 6:
+            fishList.append((graph[i][j], i, j))
 
 print(sharkX, sharkY)
 print(graph)
+fishList.sort()
+print(fishList)
 
-visited = [[False] * n for _ in range(n)]
-
+# 상어의 크기
+babyShark = 2
+# 물고기 먹은 개수
+fish = 0
+timeList = []
 
 def bfs(x, y):
-    babyShark = 2
-    fish = 0
-    timeList = []
+    global babyShark
+    global fish
+    resultList = []
     queue = deque()
     queue.append((x, y, 0))
+    visited = [[False] * n for _ in range(n)]
     while queue:
         x, y, time = queue.popleft()
-        timeList.append(time)
         for direction in range(4):
             nx = x + dx[direction]
             ny = y + dy[direction]
-            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == False:
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
                 visited[nx][ny] = True
                 if 0 < graph[nx][ny] < babyShark:
-                    fish += 1
                     queue.append((nx, ny, time + 1))
-                    if fish == babyShark:
-                        babyShark += 1
-                elif 0 < graph[nx][ny] == babyShark:
-                    queue.append((nx, ny, time))
+                    resultList.append((time + 1, nx, ny))
                 else:
-                    queue.append((nx, ny, time))
+                    queue.append((nx, ny, time + 1))
 
-    return max(timeList)
+    if resultList:
+        resultList.sort()
+        return resultList[0]
+    else:
+        return False
 
 
-print(bfs(sharkX, sharkY))
+for i in range(len(fishList)):
+    if fish < babyShark:
+        result = bfs(sharkX, sharkY)
+        if not result:
+            break
+        print("result", result)
+        # 현재 먹은 물고기는 0으로 변경
+        graph[result[1]][result[2]] = 0
+        sharkX = result[1]
+        sharkY = result[2]
+        timeList.append(result[0])
+        fish += 1
+        if fish == babyShark:
+            fish = 0
+            babyShark += 1
+
+print(timeList)
+count = 0
+for i in timeList:
+    count += i
+
+print(count)
