4a7112858a0578ff3f2c634f6fdc5396a8e3a053
유순조 <tnswh2023@gmail.com>
2022-05-22 21:24:28 +0900

    이코테

--- 구현/다시풀기/외벽점검.py
+++ 구현/다시풀기/외벽점검.py
@@ -2,35 +2,64 @@
 from itertools import permutations
 
 
-def solution(n, weak, dist):
-    print(weak)
-    length = len(weak)
-    cand = []
-    weak_point = weak + [w + n for w in weak] # 1 5 6 10 => 1 5 6 10 13 18 19 22
+# def solution(n, weak, dist):
+#     print(weak)
+#     length = len(weak)
+#     cand = []
+#     weak_point = weak + [w + n for w in weak] # 1 5 6 10 => 1 5 6 10 13 18 19 22
+#
+#     for i, start in enumerate(weak):
+#         print(start)
+#         for friends in permutations(dist):
+#             print(friends)
+#             count = 1
+#             position = start
+#             # 친구 조합 배치
+#             for friend in friends:
+#                 position += friend
+#                 # 끝 포인트까지 도달 못했을 때
+#                 if position < weak_point[i + length - 1]:
+#                     count += 1
+#                     position = [w for w in weak_point[i + 1: i + length]
+#                                 if w > position][0]
+#                     temp = []
+#                     for w in weak_point[i + 1: i + length]:
+#                         if w > position:
+#                             temp.append(w)
+#                     position = temp[0]
+#                 else:
+#                     cand.append(count)
+#                     break
+#     return min(cand) if cand else -1
 
-    for i, start in enumerate(weak):
-        print(start)
-        for friends in permutations(dist):
-            print(friends)
+def solution(n, weak, dist):
+
+    print(dist)
+    length = len(weak)
+    weak = weak + [w + n for w in weak]  # 1 5 6 10 => 1 5 6 10 13 18 19 22
+    print(weak)
+    min_count = 1e9
+
+    per_dist = list(permutations(dist))
+    for start in range(length):
+        print(per_dist)
+        for d in per_dist:
             count = 1
             position = start
-            # 친구 조합 배치
-            for friend in friends:
-                position += friend
-                # 끝 포인트까지 도달 못했을 때
-                if position < weak_point[i + length - 1]:
+            for i in range(1, length):
+                next_position = start + i
+                diff = weak[next_position] - weak[position]
+                if diff > d[count - 1]:
+                    position = next_position
                     count += 1
-                    position = [w for w in weak_point[i + 1: i + length]
-                                if w > position][0]
-                    temp = []
-                    for w in weak_point[i + 1: i + length]:
-                        if w > position:
-                            temp.append(w)
-                    position = temp[0]
-                else:
-                    cand.append(count)
-                    break
-    return min(cand) if cand else -1
+                    if count > len(dist):
+                        break
+            if count <= len(dist):
+                min_count = min(min_count, count)
+
+    if min_count == 1e9:
+        return -1
+    return min_count
 
 
 print(solution(12, [1, 5, 6, 10], [1, 2, 3, 4]))
--- 프로그래머스/ssg/two/3.py
+++ 프로그래머스/ssg/two/3.py
@@ -1,8 +1,32 @@
+from datetime import datetime, timedelta
+
+def date_range(start, end):
+    start = datetime.strptime(start, "%Y/%m/%d")
+    end = datetime.strptime(end, "%Y/%m/%d")
+    dates = [(start + timedelta(days=i)).strftime("%Y/%m/%d") for i in range((end-start).days+1)]
+    return dates
+
 def solution(masks, dates):
     answer = 0
 
+    print(masks)
 
+    print(dates)
+    date_list = []
+    for i in dates:
+        if i.find('~') != -1:
+            split = i.split('~')
+            print("split", split)
+            start_date = split[0]
+            end_date = split[1]
+            range_list = date_range(start_date, end_date)
+            date_list.extend(range_list)
+            continue
+        date_list.append(i)
 
+    date_list = list(set(date_list))
+    date_list.sort()
+    print(date_list)
     return answer
 
 
@@ -10,6 +34,6 @@ print(solution([[3200, 4], [2300, 2], [1100, 1], [4200, 6]],
                ["2022/05/02", "2022/05/01", "2022/05/07", "2022/05/05", "2022/05/08", "2022/05/13~2022/05/15",
                 "2022/05/14~2022/05/17", "2022/05/01~2022/05/02", "2022/05/16"]))
 
-print(solution([[600, 2], [500, 1], [1015, 400]], ["2023/01/01~2023/01/02", "2021/12/31"]))
-
-print(solution([[3651, 365], [10, 1]], ["2025/01/01~2025/12/31"]))
+# print(solution([[600, 2], [500, 1], [1015, 400]], ["2023/01/01~2023/01/02", "2021/12/31"]))
+#
+# print(solution([[3651, 365], [10, 1]], ["2025/01/01~2025/12/31"]))
