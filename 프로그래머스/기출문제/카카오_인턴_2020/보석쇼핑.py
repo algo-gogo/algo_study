6bd296ddfd450136dc72513cd986fce9c147cee0
ballde <67853463+YuSunjo@users.noreply.github.com>
2023-02-21 13:52:12 +0900

    코딩 테스트 문제

--- 프로그래머스/기출문제/카카오_인턴_2020/보석쇼핑.py
+++ 프로그래머스/기출문제/카카오_인턴_2020/보석쇼핑.py
@@ -15,14 +15,52 @@ from collections import Counter
 #
 #     return answer
 
+# def solution(gems):
+#     n = len(gems)
+#     gems_set = set(gems)
+#     answer = [0, n - 1]
+#     start, end = 0, 0
+#
+#     while 0 <= start < n and 0 <= end < n:
+#         if set(gems[start: end + 1]) == gems_set:
+#             if (end - start + 1) < (answer[1] - answer[0] + 1):
+#                 answer = [start, end]
+#             start += 1
+#         else:
+#             end += 1
+#
+#     answer[0] += 1
+#     answer[1] += 1
+#
+#     return answer
+
 def solution(gems):
-    answer = []
+    n = len(gems)
+    gems_set_num = len(set(gems))
+    answer = [0, n - 1]
+    start, end = 0, 0
+    gems_dict = {gems[0]: 1}
 
-    for i in range(len(gems)):
+    while 0 <= start < n and 0 <= end < n:
+        if len(gems_dict) >= gems_set_num:
+            if (end - start + 1) < (answer[1] - answer[0] + 1):
+                answer = [start, end]
+            if gems_dict[gems[start]] == 1:
+                del gems_dict[gems[start]]
+            else:
+                gems_dict[gems[start]] -= 1
 
+            start += 1
+        else:
+            end += 1
+            if end == n:
+                break
+            gems_dict[gems[end]] = gems_dict.get(gems[end], 0) + 1
+
+    answer[0] += 1
+    answer[1] += 1
 
     return answer
-
 
 print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
 # print(solution(["AA", "AB", "AC", "AA", "AC"]))
