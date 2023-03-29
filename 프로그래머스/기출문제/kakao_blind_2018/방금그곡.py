1670ed74406a77c73ab67b187310f049242492e5
ballde <67853463+YuSunjo@users.noreply.github.com>
2023-03-29 11:46:17 +0900

    코딩 테스트 문제

--- 프로그래머스/기출문제/kakao_blind_2018/방금그곡.py
+++ 프로그래머스/기출문제/kakao_blind_2018/방금그곡.py
@@ -1,6 +1,27 @@
 def solution(m, musicinfos):
     answer = ''
 
+    for music in musicinfos:
+        start, end, title, melody = music.split(',')
+        start_h, start_m = map(int, start.split(':'))
+        end_h, end_m = map(int, end.split(':'))
+        time = (end_h - start_h) * 60 + (end_m - start_m)
+        print('time', time)
+        melody = melody.replace('C#', 'c').replace('D#', 'd').replace('F#', 'f').replace('G#', 'g').replace('A#', 'a')
+        melody = list(melody)
+        melody = melody * (time // len(melody)) + melody[:time % len(melody)]
+        melody = ''.join(melody)
+        print('melody', melody)
+
+        m = m.replace('C#', 'c').replace('D#', 'd').replace('F#', 'f').replace('G#', 'g').replace('A#', 'a')
+        print('m', m)
+        if m in melody:
+            answer += title
+
+    if answer == '':
+        answer = '(None)'
+
+
     return answer
 
 
